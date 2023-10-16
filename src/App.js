ee3e316bb1f3ed0ae97fe54a8d2c0cf73ca4277d
utkarshadl1702 <81684080+utkarshadl1702@users.noreply.github.com>
2023-10-16 11:08:38 +0530

    adding backend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import logo from './logo.svg';
-import './App.css';
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+  Authenticator,
+} from "@aws-amplify/ui-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        hello from v2
-      </header>
-    </div>
+   <View>
+    <Card>
+      <Image src={logo} className='App-logo' alt='logo' />
+      <Heading level={1}>We now have Auth</Heading>
+    </Card>
+    <Authenticator>
+    {({ signOut, user }) => ( 
+        <button onClick={signOut}>Sign out</button>
+      )}
+    </Authenticator>
+   </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
